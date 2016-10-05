d24deed84836b87cf1ccfdd27ed3df9e3a8fd7ad
Diego Cavalca <diegoluizcavalca@gmail.com>
2016-10-05 08:15:06 -0300

    Atualização da pasta

--- CCO-727/Trabalho2-PSO_TSP/@/PSOX.m
+++ CCO-727/Trabalho2-PSO_TSP/@/PSOX.m
@@ -13,12 +13,12 @@
     [rows,columns]=size(distances); 
         
     %% Variaveis do PSO...
-    swarmSize = 50;
-    numIter = 1000;
+    swarmSize = 40;
+    numIter = 30;
     Vmax = 15;
     c1 = 0.9;
-    c2 = 0.05;
-    c3 = 0.05;
+    c2 = 0.8;
+    c3 = 0.8;
 
     X = zeros(swarmSize, rows); % Posicao (Particulas)
     V = cell(1, swarmSize); % Velocidade (Permutacoes)
@@ -30,23 +30,24 @@
     % Nuvem aleatoria - Metodo do Vizinho mais Proximo (Goldbarg e Luna, 2005)...
     %X(1,:) = 1:rows;
     for i=1:swarmSize;
-        particle = zeros(1, rows);
-        % Cidade inicial aleatoria...
-        particle(1) = randperm(rows, 1); 
-        for j=2:rows;
-            % k(+i) vizinhos mais proximos
-            k = 1;
-            c = particle(1);
-            target = 1;
-            while ( isempty(find(particle==c))==0 ); % Verificar se cidade ja esta na particula...
-                neighbours = distances(particle(j-1),:);
-                neighbours( neighbours==0 ) = inf;
-                [~,idx]=sort(neighbours(:));
-                c = idx(k);
-                k = k+1;
-            end;
-            particle(j) = c;
-        end;
+%         particle = zeros(1, rows);
+%         % Cidade inicial aleatoria...
+%         particle(1) = randperm(rows, 1); 
+%         for j=2:rows;
+%             % k(+i) vizinhos mais proximos
+%             k = 1;
+%             c = particle(1);
+%             target = 1;
+%             while ( isempty(find(particle==c))==0 ); % Verificar se cidade ja esta na particula...
+%                 neighbours = distances(particle(j-1),:);
+%                 neighbours( neighbours==0 ) = inf;
+%                 [~,idx]=sort(neighbours(:));
+%                 c = idx(k);
+%                 k = k+1;
+%             end;
+%             particle(j) = c;
+%         end;
+        particle = randperm(rows, rows);
         X(i,:) = particle;
     end;
 
@@ -55,6 +56,7 @@
     %% Iteracoes...
     for t=1:numIter;
         
+        disp('\n');
         fprintf('Iter %d: - gBest: %.2f \n', t, gBestScore);
         
         % Avaliar particulas...
@@ -80,55 +82,44 @@
         % Atualizar particulas (velocidade e posicao)...        
         for i=1:swarmSize;
             
+            fprintf('.');
+            
             % Coeficientes de confianca...
             w = c1*rand(1);
             alpha = c2*rand(1);
             beta = c3*rand(1);
        
-            % Deslocamento...
+            % Deslocamento... - M1
             X(i,:) = LocalSearch(X(i,:), distances);
-% % %                % Inversao
-% % %              invI = randi([1 floor(rows/2)]);
-% % % IGNORE!      invF = randi([invI+1 rows]);
-% % %              Xinv = fliplr(X(i,invI:invF));
-% % %              Vinv = SwapOperators(X(i,:), Xinv);   
-% % %              Vd = Memory(Vinv, w);
             
             % Operadores de troca... 
-             % (pBest - Xi)
-            Vp = SwapOperators(X(i,:), pBest(i,:));
-            Vp = Memory(Vp, alpha);
-             % (gBest - Xi)
+             % (gBest - Xi) - M3
             Vg = SwapOperators(X(i,:), gBest);
             Vg = Memory(Vg, beta);
+             % (pBest - Xi) - M2
+            Vp = SwapOperators(X(i,:), pBest(i,:));
+            Vp = Memory(Vp, alpha);
             
             %% Atualizando particula...
-            % Velocidade....
-            %V{i} = Vg;
             V{i} = [Vp; Vg];
             %V{i} = unique(V{i},'rows','stable');
-            % Controle de velocidade...
-            if Vmax>0;
-                if size(V{i},1) > Vmax;
-                    %V{i} = V{i}(1:Vmax,:);
-                    V{i} = V{i}(randperm(Vmax),:);
-                end;
-            end;
+%             % Controle de velocidade...
+%             if Vmax>0;
+%                 if size(V{i},1) > Vmax;
+%                     %V{i} = V{i}(1:Vmax,:);
+%                     V{i} = V{i}(randperm(Vmax),:);
+%                 end;
+%             end;
 
             % Posicao
             X(i, :) = Movement(X(i,:), V{i});
             
-            % Resetar se for melhor global
-            if isequal(X(i, :), gBest);% || isequal(X(i, :), pBest(i, :));
-              X(i, :) = randperm(rows, rows);
-            end;
-            
         end;
         
         % Atualizando confianca...
-        c1 = c1 * 0.95; 
-        c2 = c2 * 1.01; 
-        c3 = 1 - (c1+c2);
+%         c1 = c1 * 0.95; 
+%         c2 = c2 * 1.01; 
+%         c3 = 1 - (c1+c2);
         
         histBest(t) = gBestScore;
         
